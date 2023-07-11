1a44cef6a0ac348fb605ab63985dc8f9494f4638
abdul-09 <abdul.9.hussein@gmail.com>
2023-07-11 04:43:43 -0700

    write to a file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,24 +1,11 @@
 #!/usr/bin/python3
+"""
+function that write a string to text (UTF8) & return total of character written
+"""
 
-"""Write a class"""
 
-
-class Student:
-    """Defines a class Student"""
-    def __init__(self, first_name, last_name, age):
-        """initializes the attributes of class Student"""
-        self.first_name = first_name
-        self.last_name = last_name
-        self.age = age
-
-    def to_json(self, attrs=None):
-        """retrieves a dictionary representation of a Student instance"""
-        if attrs is None:
-            return self.__dict__
-
-        else:
-            d = {}
-            for attr in attrs:
-                if hasattr(self, attr):
-                    d[attr] = getattr(self, attr)
-            return d
+def write_file(filename="", text=""):
+    """ module write_file
+    """
+    with open(filename, 'w') as f:
+        return f.write(text)
