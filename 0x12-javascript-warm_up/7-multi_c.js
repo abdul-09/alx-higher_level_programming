0e228e12e8492e16703afd6fef605b355f19cf1f
abdul-09 <abdul.9.hussein@gmail.com>
2023-09-12 04:37:50 -0700

    i love c

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,14 +1,11 @@
 #!/usr/bin/node
 
-const process = require('process');
-const firstArg = process.argv[2];
-
-const x = parseInt(firstArg);
-
-if ((!isNaN(x)) {
-  for (let i = 0; i < x; i++) {
+let firstArg = process.argv[2];
+if (!firstArg || isNaN(firstArg)) {
+  console.log('Missing number of occurrences');
+} else {
+  firstArg = parseInt(firstArg);
+  for (let i = 0; i < firstArg; i++) {
     console.log('C is fun');
   }
-} else {
-  console.log('Missing number of occurrences');
 }
